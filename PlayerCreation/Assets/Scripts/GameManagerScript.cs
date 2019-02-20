4ed089533eda4c07c835ccd4e0f2b2146fcf80dd
Kubiz <kubaploch34@gmail.com>
2019-02-20 21:11:45 +0100

    Add game length select and pass data features
    Added toggles with game length options and GameManager to pass
    data between scenes

---
+++ PlayerCreation/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    //default value
+    public int GameLength;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ PlayerCreation/Assets/Scripts/SetGameLengthScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetGameLengthScript : MonoBehaviour
+{
+    public GameManagerScript GameManager;
+
+    void Awake()
+    {
+        //default value
+        GameManager.GameLength = 10;   
+    }
+
+    public void SetGameLength(int length)
+    { 
+        //TODO: set values properly after testing
+        GameManager.GameLength = length;
+    }
+}
