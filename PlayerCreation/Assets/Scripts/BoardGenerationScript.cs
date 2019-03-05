b4507e9037590095971aeb40090ff2af16a88b59
Kubiz <kubaploch34@gmail.com>
2019-03-05 17:54:05 +0100

    Add GameView scene

---
+++ PlayerCreation/Assets/Scripts/BoardGenerationScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGenerationScript : MonoBehaviour
+{
+    public GameObject TilePrefab;
+    public Transform BoardStart;
+
+    private GameManagerScript gameManager;
+
+    void Awake()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>();
+        GenerateBoard();
+    }
+
+    void GenerateBoard()
+    {
+        Instantiate(TilePrefab, BoardStart);
+    } 
+
+}
