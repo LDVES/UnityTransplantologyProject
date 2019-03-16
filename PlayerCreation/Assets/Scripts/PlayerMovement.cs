e9d9362940389828fae014625a1eb785060c4bf5
Kubiz <kubaploch34@gmail.com>
2019-03-16 18:05:40 +0100

    Add player movement
    Began adding player movement system

---
+++ PlayerCreation/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float Speed = 1f;
+    public int waypointIndex = 0;
+    public bool moveAllowed = false;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    void Update()
+    {
+        if (moveAllowed)
+            Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, Speed * Time.deltaTime);
+
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+    }
+}
